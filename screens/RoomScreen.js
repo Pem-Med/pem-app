8c4be2264dc43960ef65aa752a589873d3b8dfeb
Brayan Alfonso <brayan.alfonso95@gmail.com>
2020-11-17 02:01:09 -0500

    Modifying the chat screen UI: Changing the chat bubble

--- screens/RoomScreen.js
+++ screens/RoomScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { GiftedChat } from 'react-native-gifted-chat';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
 export default function RoomScreen() {
   const [messages, setMessages] = useState([
@@ -16,11 +16,11 @@ export default function RoomScreen() {
     // example of chat message
     {
       _id: 1,
-      text: 'Henlo!',
+      text: 'HeLlo!',
       createdAt: new Date().getTime(),
       user: {
         _id: 2,
-        name: 'Test User'
+        name: 'BRAYAN ALFONSO'
       }
     }
   ]);
@@ -30,11 +30,37 @@ export default function RoomScreen() {
     setMessages(GiftedChat.append(messages, newMessage));
   }
 
+  function renderBubble(props) {
+    return (
+      // Step 3: return the component
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            // Here is the color change
+            backgroundColor: '#6646ee'
+          }
+        }}
+        textStyle={{
+          right: {
+            color: '#fff'
+          }
+        }}
+      />
+    );
+  }
+
   return (
     <GiftedChat
       messages={messages}
       onSend={newMessage => handleSend(newMessage)}
-      user={{ _id: 1 }}
+      user={{ _id: 1, name: 'User Test' }}
+      renderBubble={renderBubble}
+      placeholder='Type your message here...'
+      showUserAvatar
+      alwaysShowSend
+      
     />
   );
 }
+
