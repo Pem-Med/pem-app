8334a6fdf98473e5a7fe78cff06c9475a899b06b
dianaliz93 <44624502+dianaliz93@users.noreply.github.com>
2020-11-24 06:41:02 -0500

    Update CMEScreen.js

--- screens/CMEScreen.js
+++ screens/CMEScreen.js
@@ -1,7 +1,6 @@
-import React, { Component, useEffect } from "react";
-import DatePicker from "react-native-datepicker";
-import * as ImagePicker from 'expo-image-picker';
-import Colors from "../constants/Colors";
+import React, { Component, useEffect } from 'react'
+import DatePicker from 'react-native-datepicker'
+import * as ImagePicker from 'expo-image-picker'
 import {
   View,
   StyleSheet,
@@ -12,34 +11,26 @@ import {
   Button,
   TouchableOpacity,
   Alert,
-<<<<<<< Updated upstream
-} from "react-native";
-import * as firebase from "firebase";
-import "firebase/firestore";
-import Firebase from "../backend/firebase";
-=======
 } from 'react-native'
 import * as firebase from 'firebase'
 import Colors from '../constants/Colors'
 import 'firebase/firestore'
 import Firebase from '../backend/firebase'
 import 'firebase/storage';
->>>>>>> Stashed changes
 
-//This will be the list of all CMEs the user has.
-let cmes = [];
+// This will be the list of all CMEs the user has.
+let cmes = []
 
-//This represents one CME the user would like to
-//add to the list of CMEs.
-let newCme = {
-  newCmeCert: "",
-  newCmeExp: "",
-  newCmePic: "",
-};
-
+// This represents one CME the user would like to
+// add to the list of CMEs.
+const newCme = {
+  newCmeCert: '',
+  newCmeExp:  '',
+  newCmePic:  '',
+}
 
 function displayOKAlert(title, message) {
-  Alert.alert(title, message);
+  Alert.alert(title, message)
 }
 
 export default class CME extends Component {
@@ -50,43 +41,40 @@ export default class CME extends Component {
   constructor(props) {
     firebase
       .database()
-      .ref("userCmes/userId:" + firebase.auth().currentUser.uid)
-      .once("value")
-      .then(function (snapshot) {
-        console.log("SNAPSHOT.VAL", snapshot.val());
-        cmes = snapshot.val() === null ? [] : snapshot.val().cmes;
+      .ref(`userCmes/userId:${firebase.auth().currentUser.uid}`)
+      .once('value')
+      .then((snapshot) => {
+        console.log('SNAPSHOT.VAL', snapshot.val())
+        cmes = snapshot.val() === null ? [] : snapshot.val().cmes
       })
-      .catch(function (err) {
-        console.log("ERROR GETTING CME DATA:", err);
-      });
-    super(props);
+      .catch((err) => {
+        console.log('ERROR GETTING CME DATA:', err)
+      })
+    super(props)
     this.state = {
-      cmes: cmes,
-    };
-    console.log("BEGINNING STATE IS", this.state.cmes);
-    this.handleCmeCert = this.handleCmeCert.bind(this);
-    this.handleCmeExp = this.handleCmeExp.bind(this);
-    this.handleCmePic = this.handleCmePic.bind(this);
-    this.addCme = this.addCme.bind(this);
+      cmes,
+    }
+    console.log('BEGINNING STATE IS', this.state.cmes)
+    this.handleCmeCert = this.handleCmeCert.bind(this)
+    this.handleCmeExp = this.handleCmeExp.bind(this)
+    this.handleCmePic = this.handleCmePic.bind(this)
+    this.addCme = this.addCme.bind(this)
   }
+
   static navigationOptions = {
-    title: "CME",
+    title: 'CME',
   };
 
   handleCmeCert(text) {
-    newCme.newCmeCert = text;
+    newCme.newCmeCert = text
   }
 
   handleCmeExp(date) {
-    newCme.newCmeExp = date;
+    newCme.newCmeExp = date
   }
 
   handleCmePic(text) {
-<<<<<<< Updated upstream
-    newPic.newCmePic = text;
-=======
     newCme.newCmePic = text
->>>>>>> Stashed changes
   }
 
   /**
@@ -96,25 +84,25 @@ export default class CME extends Component {
    * @param {string} userDate
    */
   isValidDate(userDate) {
-    console.log("USERDATE", userDate);
-    let expDateMillis = Date.parse(userDate);
-    console.log("EXPDM", expDateMillis);
-    console.log("EXPDMbool", expDateMillis === NaN);
-    console.log("EXPDMbool2", expDateMillis == NaN);
-    console.log("EXPDM2", expDateMillis);
-    console.log("EXPDMbool3", !expDateMillis);
+    console.log('USERDATE', userDate)
+    const expDateMillis = Date.parse(userDate)
+    console.log('EXPDM', expDateMillis)
+    console.log('EXPDMbool', expDateMillis === NaN)
+    console.log('EXPDMbool2', expDateMillis == NaN)
+    console.log('EXPDM2', expDateMillis)
+    console.log('EXPDMbool3', !expDateMillis)
     if (!expDateMillis) {
-      console.log("RETURNING FIRST FALSE");
+      console.log('RETURNING FIRST FALSE')
       displayOKAlert(
-        "Invalid date format",
-        "Please format your date as MM/DD/YYYY"
-      );
-      return false;
+        'Invalid date format',
+        'Please format your date as MM/DD/YYYY',
+      )
+      return false
     }
-    let today = new Date();
-    let todayMillis = Date.parse(
-      today.getMonth() + 1 + "/" + today.getDate() + "/" + today.getFullYear()
-    );
+    const today = new Date()
+    const todayMillis = Date.parse(
+      `${today.getMonth() + 1}/${today.getDate()}/${today.getFullYear()}`,
+    )
 
     if (todayMillis - expDateMillis >= 0) {
       /*
@@ -123,13 +111,13 @@ export default class CME extends Component {
       future.
       */
       displayOKAlert(
-        "Invalid date",
-        "Please make sure your date is later than today"
-      );
-      return false;
+        'Invalid date',
+        'Please make sure your date is later than today',
+      )
+      return false
     }
-    console.log("RETURNING TRUE");
-    return true;
+    console.log('RETURNING TRUE')
+    return true
   }
 
   /**
@@ -138,16 +126,6 @@ export default class CME extends Component {
    * newCme is added and this.state.cmes is set to the cmes list. It also
    * sets the userCmes in Firebase to the cmes list.
    */
-<<<<<<< Updated upstream
-  addCme() {
-    console.log("NEWCME:", newCme);
-    if (newCme.newCmeCert != "" && this.isValidDate(newCme.newCmeExp)) {
-      cmes.push({
-        cert: newCme.newCmeCert,
-        exp: newCme.newCmeExp,
-
-      });
-=======
   addCme()  {
     console.log('NEWCME:', newCme)
     if (newCme.newCmeCert != '' && this.isValidDate(newCme.newCmeExp)) {
@@ -155,30 +133,26 @@ export default class CME extends Component {
         cert: newCme.newCmeCert,
         exp:  newCme.newCmeExp,
       })
->>>>>>> Stashed changes
 
       this.setState({
-        cmes: cmes,
-      });
-      console.log("STATE IS", this.state);
+        cmes,
+      })
+      console.log('STATE IS', this.state)
 
       firebase
         .database()
-        .ref("userCmes/userId:" + firebase.auth().currentUser.uid)
+        .ref(`userCmes/userId:${firebase.auth().currentUser.uid}`)
         .set({
-          cmes: cmes,
+          cmes,
         })
-<<<<<<< Updated upstream
-        .catch(function (err) {
-          console.log("ERROR IN SETTING userCmes/userId:", err);
-        });
-=======
+        .catch((err) => {
+          console.log('ERROR IN SETTING userCmes/userId:', err)
+        })
         this.uriToBlob(newCme.newCmePic).then((blob) => this.uploadToFirebase(blob));
->>>>>>> Stashed changes
     } else {
       console.log(
-        "One or both of the fields in newCme are empty. We can't have that."
-      );
+        "One or both of the fields in newCme are empty. We can't have that.",
+      )
     }
   }
 
@@ -234,11 +208,11 @@ export default class CME extends Component {
     return (
       <View>
         <FlatList
-          style={{ marginTop: "5%", flexGrow: 0, marginBottom: "2%" }}
+          style={{ marginTop: '5%', flexGrow: 0, marginBottom: '2%' }}
           data={this.state.cmes}
-          keyExtractor={item => item.id}
+          keyExtractor={(item) => item.id}
           renderItem={(itemData) => (
-            <View style={{ flexDirection: "row" }}>
+            <View style={{ flexDirection: 'row' }}>
               <Text style={styles.cmeItem}>{(itemData.item.id, itemData.item.cert)}</Text>
               <Text style={styles.cmeItem}>{(itemData.item.id + 1, itemData.item.exp)}</Text>
             </View>
@@ -246,49 +220,41 @@ export default class CME extends Component {
           numColumns={1}
         />
 
-        <View style={{ flexDirection: "row" }}>
+        <View style={{ flexDirection: 'row' }}>
           <Text style={styles.header}>Add New Certification Here</Text>
         </View>
 
-        <View style={{ flexDirection: "row" }}>
+        <View style={{ flexDirection: 'row' }}>
           <TextInput
             style={styles.textField}
             onChangeText={this.handleCmeCert}
-            placeholder="Certification Name"
+            placeholder={'Certification Name'}
           />
         </View>
 
-        <View style={{ flexDirection: "column", justifyContent: "center" }}>
+        <View style={{ flexDirection: 'column', justifyContent: 'center' }}>
           <Text style={styles.header}>Renewal Date</Text>
           <DatePicker
             style={{
-<<<<<<< Updated upstream
-              flex: 1,
-              alignSelf: "center",
-              width: "80%",
-              marginRight: "9%",
-              marginTop: "2%"
-=======
               flex:        1,
               alignSelf:   'center',
               width:       '80%',
               marginRight: '9%',
               marginTop:   '2%',
               color: 'black',
->>>>>>> Stashed changes
             }}
-            date={this.state.date} //initial date from state
-            mode="date"
-            placeholder="Select date"
-            format="MM/DD/YYYY"
-            confirmBtnText="Confirm"
-            cancelBtnText="Cancel"
-            onDateChange={(date) => { this.setState({ date: date }, this.handleCmeExp(date)) }}
+            date={this.state.date} // initial date from state
+            mode={'date'}
+            placeholder={'Select date'}
+            format={'MM/DD/YYYY'}
+            confirmBtnText={'Confirm'}
+            cancelBtnText={'Cancel'}
+            onDateChange={(date) => { this.setState({ date }, this.handleCmeExp(date)) }}
             customStyles={{
               dateIcon: {
-                position: "absolute",
-                left: 0,
-                top: 4,
+                position:   'absolute',
+                left:       0,
+                top:        4,
                 marginLeft: 0,
               },
               dateInput: {
@@ -298,7 +264,7 @@ export default class CME extends Component {
           />
         </View>
 
-        <View style={{ flexDirection: "column", marginTop: "15%" }}>
+        <View style={{ flexDirection: 'column', marginTop: '15%' }}>
           <View style={[styles.addCmeButton, { flexDirection: 'column', backgroundColor: Colors.primaryColor }]}>
             <TouchableOpacity style={styles.buttonText} onPress={this.pickImage}>
               <Text style={styles.text}>Upload Image</Text>
@@ -309,20 +275,9 @@ export default class CME extends Component {
           </TouchableOpacity>
         </View>
       </View>
-    );
+    )
   }
 
-<<<<<<< Updated upstream
-
-const pickImage = async () => {
-  let selectedImage = await ImagePicker.launchImageLibraryAsync({
-    mediaTypes: ImagePicker.MediaTypeOptions.All,
-    allowsEditing: true,
-    quality: 1,
-  });
-  if (!selectedImage.cancelled) {
-    //setAvatar(selectedImage.uri);
-=======
   pickImage = async () => {     
     const selectedImage = await ImagePicker.launchImageLibraryAsync({       
     mediaTypes:    ImagePicker.MediaTypeOptions.All,       
@@ -333,9 +288,8 @@ const pickImage = async () => {
       console.log(selectedImage.uri)       
       this.handleCmePic(selectedImage.uri);  
      }   
->>>>>>> Stashed changes
   }
-};
+}
 
 //start
 
@@ -346,48 +300,44 @@ const pickImage = async () => {
 
 const styles = StyleSheet.create({
   textField: {
-    flex: 1,
-    fontFamily: "open-sans",
-    height: 60,
-    width: "80%",
-    textAlign: "center",
-    alignSelf: "center",
-    borderColor: "gray",
-    borderWidth: 2,
+    flex:         1,
+    fontFamily:   'open-sans',
+    height:       60,
+    width:        '80%',
+    textAlign:    'center',
+    alignSelf:    'center',
+    borderColor:  'gray',
+    borderWidth:  2,
     borderRadius: 30,
-    margin: 8,
+    margin:       8,
   },
   header: {
-    flex: 1,
-    fontSize: 20,
-    fontFamily: "open-sans",
-    textAlign: "center",
-    alignSelf: "center",
-    width: "80%",
+    flex:       1,
+    fontSize:   20,
+    fontFamily: 'open-sans',
+    textAlign:  'center',
+    alignSelf:  'center',
+    width:      '80%',
   },
   text: {
-    fontFamily: "open-sans",
-    textAlign: "center",
-    color: "white"
+    fontFamily: 'open-sans',
+    textAlign:  'center',
+    color:      'white',
   },
   addCmeButton: {
-    marginTop: 10,
-    alignSelf: "center",
-    padding: 10,
-    width: 250,
+    marginTop:       10,
+    alignSelf:       'center',
+    padding:         10,
+    width:           250,
     backgroundColor: Colors.primaryColor,
-    borderRadius: 30,
+    borderRadius:    30,
   },
   cmeItem: {
-    flex: 1,
-    fontSize: 14,
-    fontFamily: "open-sans",
-    textAlign: "center",
-    alignSelf: "center",
-    width: "80%",
+    flex:       1,
+    fontSize:   14,
+    fontFamily: 'open-sans',
+    textAlign:  'center',
+    alignSelf:  'center',
+    width:      '80%',
   },
-<<<<<<< Updated upstream
-});
-=======
 })
->>>>>>> Stashed changes
