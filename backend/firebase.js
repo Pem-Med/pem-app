f40a2a64a6d29a8dc1da1d7d5a63fe2412b33e36
vrive <vrive072@fiu.edu>
2021-03-29 17:51:08 -0400

    remove old chat functions from firebase file

--- backend/firebase.js
+++ backend/firebase.js
@@ -154,35 +154,6 @@ class Firebase {
     })
   }
 
-  get ref() {
-    return firebase.database().ref('messages')
-  }
-
-  on = callback =>
-    this.ref
-      .limitToLast(50000) //I don't want a limit for messages, so I just
-      //use a really high number, like 50000.
-      .on('child_added', snapshot => callback(this.parse(snapshot)));
-
-  parse = snapshot => {
-    const { timestamp: numberStamp, text, user } = snapshot.val();
-    const { key: _id } = snapshot;
-    const timestamp = new Date(numberStamp);
-
-    const message = {
-      _id,
-      timestamp,
-      text,
-      user,
-    };
-
-    return message;
-  }
-
-  off() {
-    this.ref.off();
-  }
-
   get uid() {
     return (firebase.auth().currentUser || {}).uid;
   }
@@ -198,21 +169,7 @@ class Firebase {
     return firebase.database.ServerValue.TIMESTAMP;
   }
 
-  send = messages => {
-    for (let i = 0; i < messages.length; i++) {
-      const { text, user } = messages[i];
-
-      const message = {
-        text,
-        user,
-        timestamp: this.timestamp,
-      };
-      this.append(message);
-    }
-  };
-
-  append = message => this.ref.push(message);
-
+  
   /**
  * Adds the CME info to the list.
  */
